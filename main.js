b440ed4afa004859a1f10bf858a0a72b9e2cf58f
ringoXD <nep360i@gmail.com>
2024-08-21 04:17:27 +0000

    ok

---
+++ main.js
@@ -0,0 +1,29 @@
+'use strict'
+const { Client } = require('discord.js-selfbot-v13')
+
+const client = new Client()
+const config = require('./config.json')
+
+client.once('ready', async (client) => {
+    console.log(`${client.user.username} is ready.`)
+})
+
+client.on('messageCreate', async (msg) => {
+    if (msg.content === '!purge10') {
+        try {
+            const channel = await msg.channel;
+            channel.messages.fetch({ limit: 10 }).then(messages => {
+                console.log(`Received ${messages.size} messages`);
+                //Iterate through the messages here with the variable "messages".
+                messages.forEach(message => {
+                    console.log(message.content)
+                    message.delete()
+                })
+            })
+        } catch (e) {
+            await msg.reply(e)
+        }
+    }
+})
+
+client.login(config.token)
